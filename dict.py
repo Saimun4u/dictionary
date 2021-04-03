9d6884c0c8c5ff95c491052d62841e14c3586888
Saimun <saimun4u@gmail.com>
2021-04-03 13:07:27 -0500

    1st step: An empty array for data 2nd step: Create a function to take input and search for the input in the dictionary 3rd step: Simplified the function and the logic 4th step: Optimize the output for better presentation

--- dict.py
+++ dict.py
@@ -1,4 +1,6 @@
 import json
+import difflib
+from difflib import get_close_matches
 
 data = json.load(open('data.json'))
 
@@ -32,8 +34,17 @@ def translate(w):
     w = w.lower()
     if w in data:
         return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        user_response = input(f'Did you mean the word {get_close_matches(w, data.keys())[0]}. Enter y for yes and n for no: ')
+        if user_response == 'y':
+            return data[get_close_matches(w, data.keys())[0]]
+        elif user_response == 'n':
+            return "The word is not found in the dictionary"
+        else:
+            return "Please enter y or n only"
     else:
         return 'The word is not in the dictionary'
+
 
 word = input('Enter the meaning of the word you want to search: ')
 
@@ -43,7 +54,15 @@ word = input('Enter the meaning of the word you want to search: ')
 #         print(translate(word))
 # print('The word is not in the dictionary')
 
-print(translate(word))
+meaning = translate(word)
+
+
+if type(meaning) == list:
+    for option in meaning:
+        print(option)
+else:
+    print(meaning)
+
 
 
 
