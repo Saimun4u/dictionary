bb95dac61ce94f78b8df4cc5f32f5d5678515719
Saimun <saimun4u@gmail.com>
2021-04-02 22:58:12 -0500

    1st step: An empty array for data 2nd step: Create a function to take input and search for the input in the dictionary

--- dict.py
+++ dict.py
@@ -1,1 +1,19 @@
-data = []
+import json
+
+data = json.load(open('data.json'))
+
+
+def take_input():
+
+    while True:
+
+        word = input('Enter the word you want to search: ')
+
+        for w, m in data.items():
+            if word == w:
+                print(data[w])
+
+            # elif word != w:
+            #     print(f'The word {word} cannot be found in the dictionary')
+
+take_input()
