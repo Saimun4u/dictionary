1a9145cfba7604bc8c636d77bb2700ca147046d8
Saimun <saimun4u@gmail.com>
2021-04-03 11:01:49 -0500

    1st step: An empty array for data 2nd step: Create a function to take input and search for the input in the dictionary 3rd step: Simplified the function and the logic

--- dict.py
+++ dict.py
@@ -3,17 +3,47 @@ import json
 data = json.load(open('data.json'))
 
 
-def take_input():
+#My solution
 
-    while True:
+# def take_input():
+#
+#     while True:
+#
+#         word = input('\n Enter the word you want to search: ')
+#         print("To try another word, enter 'y' or else 'n' to quit")
+#
+#         for w, m in data.items():
+#             if word == w:
+#                 print(data[w])
+#
+#         if word == 'n':
+#             print('Thanks for using the dictionary')
+#             break
+#
+#             # elif word != w:
+#             #     print(f'The word {word} cannot be found in the dictionary')
+#
+# take_input()
 
-        word = input('Enter the word you want to search: ')
 
-        for w, m in data.items():
-            if word == w:
-                print(data[w])
+#Ardit's solution
 
-            # elif word != w:
-            #     print(f'The word {word} cannot be found in the dictionary')
+def translate(w):
+    w = w.lower()
+    if w in data:
+        return data[w]
+    else:
+        return 'The word is not in the dictionary'
 
-take_input()
+word = input('Enter the meaning of the word you want to search: ')
+
+
+# for w, m in data.items():
+#     if word == w:
+#         print(translate(word))
+# print('The word is not in the dictionary')
+
+print(translate(word))
+
+
+
